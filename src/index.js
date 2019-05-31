04ae8e9caa9344d8ef74fa737a62444f76d97709
ohager <oliver.hager@dextra-sw.com>
2019-05-30 23:21:28 -0300

    routing adjustment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
+import { createHashHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
@@ -9,13 +9,12 @@ import Admin from "layouts/Admin.jsx";
 
 import "assets/css/material-dashboard-react.css?v=1.6.0";
 
-const hist = createBrowserHistory();
+const hist = createHashHistory();
 
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/admin" component={Admin} />
-      <Redirect from="/" to="/admin/dashboard" />
+      <Route path="/" component={Admin} />
     </Switch>
   </Router>,
   document.getElementById("root")
