b06ef52c1d407056c2368f7da2a968e4677f5519
ohager <oliver.hager@dextra-sw.com>
2019-05-30 23:15:28 -0300

    Updates

---
+++ precache-manifest.6ad14f3cbe26932646a11298c39041e3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34734e81c9143ce80c4f7078ac34899",
+    "url": "/phoenix-analytics/index.html"
+  },
+  {
+    "revision": "d943414be24709027331",
+    "url": "/phoenix-analytics/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "2265743d3f9847534f12",
+    "url": "/phoenix-analytics/static/css/main.f313c2a0.chunk.css"
+  },
+  {
+    "revision": "d943414be24709027331",
+    "url": "/phoenix-analytics/static/js/2.c2cdb39c.chunk.js"
+  },
+  {
+    "revision": "2265743d3f9847534f12",
+    "url": "/phoenix-analytics/static/js/main.f0c45c2f.chunk.js"
+  },
+  {
+    "revision": "c6911ab2b63c51adc210",
+    "url": "/phoenix-analytics/static/js/runtime~main.f3414984.js"
+  },
+  {
+    "revision": "8ee97699bbcfb6ae2a0f84a922999c56",
+    "url": "/phoenix-analytics/static/media/decentralized.8ee97699.jpg"
+  },
+  {
+    "revision": "09a5b3658efeee932cc5799f9afb7b2e",
+    "url": "/phoenix-analytics/static/media/decentralized_bg.09a5b365.jpg"
+  },
+  {
+    "revision": "1cbfdd80dcf8600ae741acccb9e42f52",
+    "url": "/phoenix-analytics/static/media/logo_white.1cbfdd80.svg"
+  },
+  {
+    "revision": "ca9f7b1c445f93294fed5d8df1a60c74",
+    "url": "/phoenix-analytics/static/media/phoenix_logo.ca9f7b1c.png"
+  }
+]);
