8abfb7ff0b1da0c087b2397ef20186be13a52c0b
ohager <oliver.hager@dextra-sw.com>
2019-06-17 21:08:45 -0300

    fixed bff cors problem for mutation denial

--- bff/api/github.js
+++ bff/api/github.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 
+function denyMutations (proxyReq, req, res) {
+  if(req.body.query && /mutation\W.+?{/gmi.test(req.body.query)){
+    res.status(403).end("Mutations are not allowed")
+  }
+}
+
 const proxyOptions = {
   target: "https://api.github.com",
   pathRewrite: {
@@ -9,19 +15,14 @@ const proxyOptions = {
   changeOrigin: true,
   headers: {
     Authorization: `Bearer ${process.env.GH_TOKEN}`
-  }
+  },
+  onProxyReq: denyMutations
+
 };
 
 const app = express();
 
-app.use('*', (req, res) => {
-
-  if(/mutation\W.+?{/gmi.test(req.body.query)){
-    return res.sendStatus(403)
-  }
-
-  return proxy(proxyOptions)(req, res)
-});
+app.use('*',proxy(proxyOptions));
 
 
 module.exports = app;
